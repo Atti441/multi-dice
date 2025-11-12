bf93b5d3f3b9fb890d2b342497186aabf362c392
Atti441 <122789514+Atti441@users.noreply.github.com>
2025-11-12 11:33:44 +0100

    pxt.json, main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
 let dice = 0
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
-    
+input.onGesture(Gesture.Shake, function () {
+    music.play(music.stringPlayable("C5 G A E F C D C ", 190), music.PlaybackMode.UntilDone)
     dice = randint(1, 5)
     basic.showNumber(dice)
+    basic.showIcon(IconNames.Yes)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
+	
 })
